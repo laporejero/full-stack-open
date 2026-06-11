c692d10723eda33373da4dd9e323ee91c947b432
laporejero <laporejero1297@gmail.com>
2026-06-11 23:53:30 +0800

    completed Exercise 4.5*: Helper Functions and Unit Tests, step 3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,4 +6,15 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+    const blog = blogs.reduce((favorite, current) => 
+        favorite.likes > current.likes ? favorite : current
+    )
+    return { 'title': blog.title, 'author': blog.author, 'likes': blog.likes }
+}
+
+module.exports = { 
+    dummy, 
+    totalLikes, 
+    favoriteBlog 
+}
