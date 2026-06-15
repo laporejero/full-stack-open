b6c700a5e2664abcc341715a4606c62e07a00b7f
laporejero <laporejero1297@gmail.com>
2026-06-15 15:23:10 +0800

    completed Exercises 4.9-12: Blog List tests

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -3,32 +3,18 @@ const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const helper = require('./test_helper.js')
 const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-const initialBlogs = [
-    {
-        title: "Learning Full Stack Open",
-        author: "Jero",
-        url: "https://fullstackopen.com",
-        likes: 5
-    },
-    {
-        
-        title: "React State Management Basics",
-        author: "Alice Johnson",
-        url: "https://example.com/react-state",
-        likes: 20
-    }
-]
-
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
-    await blogObject.save()
-    blogObject = new Blog(initialBlogs[1])
-    await blogObject.save()
+    
+    const blogObjects = helper.initialBlogs
+        .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
 })
 
 test('blogs are returned as json', async () => {
@@ -38,10 +24,90 @@ test('blogs are returned as json', async () => {
         .expect('Content-Type', /application\/json/)
 })
 
-test('all blogs are return', async () => {
+test('blog list returns the correct amount of blog posts', async () => {
     const response = await api.get('/api/blogs')
 
-    assert.strictEqual(response.body.length, initialBlogs.length)
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('blogs have id property', async () => {
+    const response = await api.get('/api/blogs')
+    console.log(response.body)
+    assert(response.body[0].id)
+})
+
+test('a valid blog can be added', async () => {
+    const newBlog = {
+        title: 'New Blog',
+        author: 'Fullstackopen',
+        url: 'http://www.fullstackopen.com/',
+        likes: 10,
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+    const blogs = blogsAtEnd.map(blog => blog.title)
+    assert(blogs.includes('New Blog'))
+})
+
+test('blog with "likes" property missing will default to 0', async () => {
+    const newBlog = {
+        title: 'Test Blog',
+        author: 'Tester',
+        url: 'http://test.com'
+    }
+
+    const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogs = await helper.blogsInDb()
+    const savedBlog = blogs.find(blog => blog.title === 'Test Blog')
+
+    assert.strictEqual(savedBlog.likes, 0)
+})
+
+test('new blog without title cannot be added', async () => {
+    const newBlog = {
+        url: 'http://blog.com',
+        author: 'fullstackopen',
+        likes: 5
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+    
+    const blogs = await helper.blogsInDb()
+
+    assert.strictEqual(blogs.length, helper.initialBlogs.length)
+})
+
+test('new blog without url cannot be added', async () => {
+    const newBlog = {
+        title: 'Test blog',
+        author: 'fullstackopen',
+        likes: 5
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+    
+    const blogs = await helper.blogsInDb()
+
+    assert.strictEqual(blogs.length, helper.initialBlogs.length)
 })
 
 after(async () => {
--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,68 +1,8 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const { listWithOneBlog, initialBlogs } = require('./test_helper')
 
-const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 5,
-      __v: 0
-    }
-]
-
-const blogs = [
-    {
-        _id: "5a422a851b54a676234d17f7",
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 7,
-        __v: 0
-    },
-    {
-        _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-        __v: 0
-    },
-    {
-        _id: "5a422b3a1b54a676234d17f9",
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
-        __v: 0
-    },
-    {
-        _id: "5a422b891b54a676234d17fa",
-        title: "First class tests",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-        likes: 10,
-        __v: 0
-    },
-    {
-        _id: "5a422ba71b54a676234d17fb",
-        title: "TDD harms architecture",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-        likes: 0,
-        __v: 0
-    },
-    {
-        _id: "5a422bc61b54a676234d17fc",
-        title: "Type wars",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-        likes: 2,
-        __v: 0
-    }
-]
 
 test('dummy returns one', () => {
   const blogs = []
@@ -80,7 +20,7 @@ describe('total likes', () => {
 
 describe('favorite blog', () => {
   test('blog with the most likes', () => {
-    const result = listHelper.favoriteBlog(blogs)
+    const result = listHelper.favoriteBlog(initialBlogs)
     assert.deepStrictEqual(result, {
         title: "Canonical string reduction",
         author: "Edsger W. Dijkstra",
@@ -91,7 +31,7 @@ describe('favorite blog', () => {
 
 describe('most blogs', () => {
   test('author with largest number of blogs', () => {
-    const result = listHelper.mostBlogs(blogs)
+    const result = listHelper.mostBlogs(initialBlogs)
 
     assert.deepStrictEqual(result, {
       author: "Robert C. Martin",
@@ -102,7 +42,7 @@ describe('most blogs', () => {
 
 describe('most likes', () => {
   test('author whose blog posts have the largest amount of likes', () => {
-    const result = listHelper.mostLikes(blogs)
+    const result = listHelper.mostLikes(initialBlogs)
 
     assert.deepStrictEqual(result, {
       author: "Edsger W. Dijkstra",
@@ -110,3 +50,5 @@ describe('most likes', () => {
     })
   })
 })
+
+module.exports = { blogs }
