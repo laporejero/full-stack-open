05740b57e1202ad006edfbdb92808f48eb6c4296
laporejero <laporejero1297@gmail.com>
2026-06-17 20:15:47 +0800

    completed Exercise 4.19: Blog List Expansion, step 7

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
@@ -8,13 +9,24 @@ blogsRouter.get('/', async (request, response, next) => {
     response.json(blogs)
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
-
-    const user = await User.findById(body.userId)
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     if (!user) {
-        return response.status(400).json({ error: 'userId missing or not valid' })
+        return response.status(400).json({ error: 'UserId missing or not valid' })
     }
 
     if (!body.title || !body.url) {
