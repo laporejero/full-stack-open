8b7f04ed6c66f815d5191c96094e860db62b2e76
laporejero <laporejero1297@gmail.com>
2026-06-19 16:09:03 +0800

    completed Exercise 5.7 Blog List Frontend, step 7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [viewBlog, setViewBlog] = useState(false)
+
+  const toggleView = () => {
+    setViewBlog(!viewBlog)
+  }
+
+  const blogStyle = {
+    padding: 5,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} 
+        <button onClick={toggleView}>{ viewBlog ? 'hide' : 'view' }</button>
+        <div style={{ display: viewBlog ? '' : 'none' }}>
+          <span>{blog.url}</span> <br />
+          <span>{blog.likes} <button>like</button></span> <br />
+          <span>{blog.user.name}</span>
+        </div>
+      </div>
+    </div>
+  )  
+}
 
 export default Blog
