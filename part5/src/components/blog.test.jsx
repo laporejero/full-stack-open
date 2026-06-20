eefd8d607381db879d07727b58a347c07aa55a5f
laporejero <laporejero1297@gmail.com>
2026-06-20 15:11:46 +0800

    completed Exercise 5.14: Blog List Tests, step 2

--- part5/src/components/blog.test.jsx
+++ part5/src/components/blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test("renders blog's title and author", () => {
@@ -22,4 +23,27 @@ test("renders blog's title and author", () => {
     expect(otherDetails).toHaveTextContent('https://example.com')
     expect(otherDetails).toHaveTextContent('10')
     expect(otherDetails).not.toBeVisible()
+})
+
+test('url and likes are shown when button is clicked', async () => {
+    const blog = {
+        title: 'Testing React apps',
+        author: 'fullstackopen',
+        url: 'https://example.com',
+        likes: 10,
+        user: {
+            name: 'Admin'
+        }
+    }
+
+    render(<Blog blog={blog} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const url = screen.getByText('https://example.com')
+    const likes = screen.getByText('10')
+    expect(url).toBeVisible()
+    expect(likes).toBeVisible()
 })
