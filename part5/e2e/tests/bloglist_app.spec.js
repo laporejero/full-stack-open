70bc33b1ebaca33165fcafc25b88c7612430df8b
laporejero <laporejero1297@gmail.com>
2026-06-22 17:01:53 +0800

    completed Exercise 5.18: Blog List End To End Testing, step 2

--- part5/e2e/tests/bloglist_app.spec.js
+++ part5/e2e/tests/bloglist_app.spec.js
@@ -1,7 +1,17 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('blog app', () => {
-    beforeEach(async ({ page }) => {
+    beforeEach(async ({ page, request }) => {
+        await request.post('http://localhost:3003/api/testing/reset')
+        await request.post('http://localhost:3003/api/users', {
+            data: {
+                name: 'Test User',
+                username: 'user',
+                password: 'test'
+            }
+        })
+
         await page.goto('http://localhost:5173')
     })
 
@@ -11,5 +21,17 @@ describe('blog app', () => {
         await expect(page.getByLabel('username')).toBeVisible()
         await expect(page.getByLabel('password')).toBeVisible()
         await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+    })
+
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await loginWith(page, 'user', 'test')
+            await expect(page.getByText('Test User logged in')).toBeVisible()
+        })
+
+        test('fails with wrong credentials', async ({ page }) => {
+            await loginWith(page, 'user', 'wrong')
+            await expect(page.getByText('wrong username or password')).toBeVisible()
+        })
     })
 })
