16a24a42b85eaa52883a38daf21697f06dfebe9e
laporejero <laporejero1297@gmail.com>
2026-06-26 16:50:40 +0800

    completed Exercise 6.9 anecdotes, step8

--- part6/zustand-anecdotes/src/store.js
+++ part6/zustand-anecdotes/src/store.js
@@ -1,8 +1,9 @@
 
 import { create } from 'zustand'
 import anecdoteService from './services/anecdotes'
+import anecdotes from './services/anecdotes'
 
-const useAnecdoteStore = create((set) => ({
+const useAnecdoteStore = create((set, get) => ({
   anecdotes: [],
   filter: '',
   actions: {
@@ -10,21 +11,21 @@ const useAnecdoteStore = create((set) => ({
       const newAnecdote = await anecdoteService.createNew(content)
       set(state => ({ anecdotes: state.anecdotes.concat(newAnecdote) }))
     },
-    vote: id => set(state => {
-      const updatedAnecdotes = state.anecdotes.map(anecdote =>
-        anecdote.id === id 
-          ? { ...anecdote, votes: anecdote.votes + 1 } 
-          : anecdote
+    vote: async (id) => {
+      const anecdote = get().anecdotes.find(anecdote => anecdote.id === id)
+      const updated = await anecdoteService.update(
+        id, { ...anecdote, votes: anecdote.votes + 1 }
       )
-
-      return {
-        anecdotes: updatedAnecdotes.toSorted((a, b) => b.votes - a.votes)
-      }
-    }),
+      set(state => ({
+        anecdotes: state.anecdotes
+          .map(anecdote => anecdote.id === id ? updated : anecdote)
+          .toSorted((a, b) => b.votes - a.votes)
+      }))
+    },
     setFilter: value => set(() => ({ filter: value })),
     initialize: async () => {
       const anecdotes = await anecdoteService.getAll()
-      set(() => ({ anecdotes }))
+      set(() => ({ anecdotes: anecdotes.toSorted((a, b) => b.votes - a.votes) }))
     }
   },
 }))
