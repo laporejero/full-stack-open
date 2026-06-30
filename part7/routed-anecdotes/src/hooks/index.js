73673e38122a769a70fdc1ed8ee92b74ba9a712e
laporejero <laporejero1297@gmail.com>
2026-06-30 17:14:43 +0800

    completed Exercise 7.4: useAnecdotes, step1

--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import anecdoteService from '../services/anecdotes'
 
 export const useField = (type) => {
   const [value, setValue] = useState('')
@@ -21,6 +22,12 @@ export const useField = (type) => {
 
 // modules can have several named exports
 
-export const useAnotherHook = () => {
-  // ...
+export const useAnecdotes = () => {
+  const [anecdotes, setAnecdotes] = useState([])
+  
+  useEffect(() => {
+    anecdoteService.getAll().then(data => setAnecdotes(data))
+  }, [])
+
+  return {anecdotes, setAnecdotes}
 }
